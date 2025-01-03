326af44f1506223d84239b76d753602812156e9d
Helen Kochetkova <helkochetkova@gmail.com>
2025-01-02 18:51:48 -0800

    banner

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -8,7 +8,7 @@ export const Banner = () => {
   return (
     <div
       className={cn(
-        "relative isolate flex items-center gap-x-6 overflow-hidden bg-gray-50 px-6 py-1.5 sm:px-3.5 sm:before:flex-1"
+        "relative isolate flex justify-center items-center gap-x-6 overflow-hidden bg-gray-50 px-6 py-1.5 sm:px-3.5 sm:before:flex-1"
       )}
     >
       <div className="absolute top-0 left-0 h-full w-full bg-gradient-to-r from-[#fba776] to-[#9089fc] opacity-50 blur-2xl pointer-events-none"></div>
